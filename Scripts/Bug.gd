6a815a4f44ee50f5abd964eef72e803f43b70dd2
Vitor Henrique <vitorhenrique908@gmail.com>
2021-04-17 14:32:42 -0300

    Corrigido o erro do áudio repetido dos bugs

--- Scripts/Bug.gd
+++ Scripts/Bug.gd
@@ -1,14 +1,11 @@
 extends Area2D
 
 func _on_Bug_body_entered(body:Node) -> void:
-	if body.name == "Player" and visible:	
+	if body.name == "Player" and visible:
+		visible = false	
 		BugStats.found_bug()
 		PlayerStats.increment_score()
-		
-		if visible:
-			$AudioStreamPlayer.play()
-			visible = false
-			
+		$AudioStreamPlayer.play()
 		yield($AudioStreamPlayer, "finished")	
 		queue_free()
 
