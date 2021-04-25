f2ad28c5619392e87b724fe6ea9d74dbb0a948fa
jonathan <jonathanga2011@gmail.com>
2021-04-25 05:16:36 -0300

    adicionar funcoes no player_stats

--- Scripts/PlayerStats.gd
+++ Scripts/PlayerStats.gd
@@ -1,16 +1,23 @@
 extends Node
 
 var score = 0
+var children = 0
 var initial_position
 
 func increment_score():
 	self.score += 1 
 
-func score_count():
-	return str(get_score()) + "/"+ str(10)
+func decrement_score():
+	self.score -= 1 	
+
+func increment_children():
+	self.children += 1 
 
 func get_score():
 	return self.score
+
+func get_children():
+	return self.children
 
 func set_initial_position(position):
 	self.initial_position = position
